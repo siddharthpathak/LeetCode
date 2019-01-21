566ddd2609de22f58dce751201d105366239be82
Siddharth Pathak <siddharthpathak7@gmail.com>
2019-01-21 02:13:21 -0500

    Update flip_binary_tree.py

--- flip_binary_tree.py
+++ flip_binary_tree.py
@@ -1,11 +1,11 @@
-"""
+'''
 Leetcode: https://leetcode.com/problems/flip-equivalent-binary-trees/
 
 Approach #1: Traverse both the trees recursively and check if the left child is equal to the left or right child of the other tree
 
 Approach #2: As we know that each number is going to be unique, then that number should have the same parent in both the trees. 
 Traverse the first tree and store the node value and parent in an array. While traversing the second tree check if the parent is the same.
-"""
+'''
 
 # Definition for a binary tree node.
 # class TreeNode(object):
